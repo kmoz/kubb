11327d5cf72f552687beb0b7bce8a03df24c3b3d
kmoz <kamil.mozgala@gmail.com>
2017-03-04 00:49:38 +0100

    v3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-// Nav collapse
 $(document).ready(function(){
+  // Nav collapse
   $(".nav-trigger").on("click", function() {
     $(".nav").toggleClass('open')
   })
+  //Change navbar bgcolor on scroll
+  $(window).scroll(function(){
+    if ($(document).scrollTop() > 50){
+      $("nav").addClass('navbar-scroll')
+    }
+    else {
+      $('nav').removeClass('navbar-scroll')
+    }
+  })
 });
-
-
-//Change navbar bgcolor on scroll
-$(document).ready(function(){
-      $(window).scroll(function() {
-        if ($(document).scrollTop() > 200) {
-          $(".navbar").css("background-color", "rgba(0, 0, 0, 1)");
-        } else {
-          $(".navbar").css("background-color", "rgba(0, 0, 0, 0.5)");
-        }
-      });
-    });
